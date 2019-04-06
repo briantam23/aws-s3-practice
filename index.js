98ac5fe55d003c4556f1c4b925ada644aa296536
briantam23 <briantam23@gmail.com>
2019-04-06 09:31:08 -0400

    able to upload to AWS

--- index.js
+++ index.js
@@ -9,3 +9,21 @@ AWS.config.update({
     accessKeyId,
     secretAccessKey
 })
+
+const s3 = new AWS.S3();
+const filePath = './data/file.txt';
+
+//configuring parameters
+const params = {
+    Bucket: 'btam-s3-practice',
+    Body: fs.createReadStream(filePath),
+    Key: 'folder/' + Date.now() + '_' + path.basename(filePath)
+}
+
+s3.upload(params, (err, data) => {
+    //handle error
+    if(err) console.log('Error', err);
+
+    //success
+    if(data) console.log('Uploaded in:', data.Location);
+})
